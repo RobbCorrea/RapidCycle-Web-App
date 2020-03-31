7baf7cba22a0a4438e6377f693be381a85c74a1b
robbcorrea <51095085+RobbCorrea@users.noreply.github.com>
2020-03-30 19:23:39 -0600

    testing menuCollapsed and setMenuCollapsed

--- src/components/Collector/MenuTop/MenuTop.js
+++ src/components/Collector/MenuTop/MenuTop.js
@@ -7,6 +7,7 @@ import "../../../scss/MenuTop.scss";
 
 function MenuTop(props) {
   console.log(props);
+  const { menuCollapsed, setMenuCollapsed } = props;
   return (
     <div className="menu-top">
       <div className="menu-top__left">
@@ -15,7 +16,7 @@ function MenuTop(props) {
           src={RCLeafLogo}
           alt="Rapid Cycle Green Leaf Logo"
         />
-        <Button type="link" onClick={() => console.log("Cliiick!")}>
+        <Button type="link" onClick={() => setMenuCollapsed(true)}>
           <MenuUnfoldOutlined />
         </Button>
       </div>
