43ceb59b3bd4c1f582936d50402cc81f06545a1b
RobbCorrea <robertocorreaalpizar@hotmail.com>
2020-04-23 01:14:23 -0500

    Add Form fields with scss styles to AddUserForm. Add AddUserForm component to ListUsers

--- src/components/Collector/Users/ListUsers/ListUsers.js
+++ src/components/Collector/Users/ListUsers/ListUsers.js
@@ -18,6 +18,7 @@ import {
 //Components
 import Modal from "../../../Modal";
 import EditUserForm from "../EditUserForm";
+import AddUserForm from "../AddUserForm";
 import {
   getAvatarApi,
   activateUserApi,
@@ -29,12 +30,25 @@ import NoAvatar from "../../../../assets/images/avatar.png";
 
 import "../../../../scss/ListUsers.scss";
 
+const { confirm } = ModalAntd;
+
 export default function ListUsers(props) {
   const { usersActive, usersInactive, setReloadUsers } = props;
   const [viewUsersActives, setViewUsersActives] = useState(true);
   const [isVisibleModal, setIsVisibleModal] = useState(false);
   const [modalTitle, setModalTitle] = useState("");
   const [modalContent, setModalContent] = useState(null);
+
+  const addUserModal = () => {
+    setIsVisibleModal(true);
+    setModalTitle("Creando nuevo usuario");
+    setModalContent(
+      <AddUserForm
+        setIsVisibleModal={setIsVisibleModal}
+        setReloadUsers={setReloadUsers}
+      />
+    );
+  };
 
   return (
     <div className="list-users">
@@ -48,7 +62,7 @@ export default function ListUsers(props) {
             {viewUsersActives ? "Usuarios activos" : "Usuarios inactivos"}
           </span>
         </div>
-        <Button type="primary" onClick={() => console.log("Creando usuario")}>
+        <Button type="primary" onClick={addUserModal}>
           Nuevo usuario
           <UserAddOutlined />
         </Button>
@@ -155,7 +169,7 @@ function UserActive(props) {
   const showDeleteConfirm = () => {
     const accessToken = getAccessTokenApi();
 
-    window.confirm({
+    confirm({
       title: "Deleting user. Eliminando usuario",
       content: `Are you sure? ¿Estas seguro que quieres eliminar a ${user.email}?`,
       okText: "Eliminar",
@@ -253,7 +267,7 @@ function UserInactive(props) {
   const showDeleteConfirm = () => {
     const accessToken = getAccessTokenApi();
 
-    window.confirm({
+    confirm({
       title: "Deleting user. Eliminando usuario",
       content: `Are you sure? ¿Estas seguro que quieres eliminar a ${user.email}?`,
       okText: "Eliminar",
@@ -300,31 +314,6 @@ function UserInactive(props) {
 }
 
 /*
-  const showDeleteConfirm = () => {
-    const accesToken = getAccessTokenApi();
-
-    confirm({
-      title: "Eliminando usuario",
-      content: `¿Estas seguro que quieres eliminar a ${user.email}?`,
-      okText: "Eliminar",
-      okType: "danger",
-      cancelText: "Cancelar",
-      onOk() {
-        deleteUserApi(accesToken, user._id)
-          .then(response => {
-            notification["success"]({
-              message: response
-            });
-            setReloadUsers(true);
-          })
-          .catch(err => {
-            notification["error"]({
-              message: err
-            });
-          });
-      }
-    });
-  }; 
   
   EL LIST ITEM QUE CORTAMOS DEL RETURN DE USERSACTIVE AQUI MISMO
   <List.Item
@@ -378,29 +367,4 @@ function UserInactive(props) {
       });
   };
 
-  const showDeleteConfirm = () => {
-    const accesToken = getAccessTokenApi();
-
-    confirm({
-      title: "Eliminando usuario",
-      content: `¿Estas seguro que quieres eliminar a ${user.email}?`,
-      okText: "Eliminar",
-      okType: "danger",
-      cancelText: "Cancelar",
-      onOk() {
-        deleteUserApi(accesToken, user._id)
-          .then(response => {
-            notification["success"]({
-              message: response
-            });
-            setReloadUsers(true);
-          })
-          .catch(err => {
-            notification["error"]({
-              message: err
-            });
-          });
-      }
-    });
-  };
   */
